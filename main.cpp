6bd73ae200dcffb7c70b8cc7a98cfd219d72436a
Leomalinen02 <ldmalinen@csuchico.edu>
2024-09-25 11:30:41 -0700

    fixed issue 2 for quiz

--- main.cpp
+++ main.cpp
@@ -4,24 +4,26 @@
 using std::cout, std::cin, std::endl, std::string, std::vector;
 
 int main(){
-  string input = "";
-  vector<string> list;
+    string input = "";
+    vector<string> list;
 
-  do{
-    if( list.size() == 0 ){
-      cout << "What is your favorite?\n";
+    do{
+        if( list.size() == 0 ){
+            cout << "What is your favorite?\n";
+        }
+        else{
+            cout << "What is your next favorite?\n";
+        }
+        std::getline(cin, input);
+        if (input != "done") {
+            list.push_back(input);
+        }
+    }while( input != "done" );
+
+    cout << "Your favorite list:\n";
+    for(int i = 0; i < list.size(); i++){
+        cout << list.at(i) << endl;
     }
-    else{
-      cout << "What is your next favorite?\n";
-    }
-    cin >> input;
-    list.push_back(input);
-  }while( input != "done" );
 
-  cout << "Your favorite list:\n";
-  for(int i = 0; i < list.size(); i++){
-    cout << list.at(i) << endl;
-  }
-
-  return 0;
+    return 0;
 }
