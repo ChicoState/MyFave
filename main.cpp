9e04a53ea6e0562268e6eca3466ee67c151bbb8c
Kent Menigoz <klmenigoz@csuchico.edu>
2024-09-25 11:35:11 -0700

    forgot to add message, resolved #2

--- main.cpp
+++ main.cpp
@@ -7,16 +7,19 @@ int main(){
   string input = "";
   vector<string> list;
 
-  do{
-    if( list.size() == 0 ){
-      cout << "What is your favorite?\n";
-    }
-    else{
-      cout << "What is your next favorite?\n";
-    }
-    cin >> input;
-    list.push_back(input);
-  }while( input != "done" );
+    do {
+        if( list.size() == 0 ){
+            cout << "What is your favorite?\n";
+        } else {
+            cout << "What is your next favorite?\n";
+        }
+        getline(cin, input);
+
+        if (input != "done") {
+            list.push_back(input);
+        }
+        }while( input != "done" ); // moved this to after check for "done" input
+
 
   cout << "Your favorite list:\n";
   for(int i = 0; i < list.size(); i++){
