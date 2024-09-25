d2f1dea2688da3bc0fa4151f43803f547e38f5a3
leszakal <lesakal@protonmail.com>
2024-09-25 11:29:05 -0700

    Add input handling for including spaces; exclude done from list, closes #2

--- main.cpp
+++ main.cpp
@@ -7,16 +7,19 @@ int main(){
   string input = "";
   vector<string> list;
 
-  do{
+  while(1) {
     if( list.size() == 0 ){
       cout << "What is your favorite?\n";
     }
     else{
       cout << "What is your next favorite?\n";
     }
-    cin >> input;
+    getline(cin, input);
+    if(input == "done") {
+      break;
+    }
     list.push_back(input);
-  }while( input != "done" );
+  }
 
   cout << "Your favorite list:\n";
   for(int i = 0; i < list.size(); i++){
