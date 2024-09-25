f104e6c5be81dc24f9a010334bdd90a1f1abe3f9
Nathaniel Martinez <njmartinez2@csuchico.edu>
2024-09-25 11:41:37 -0700

    Added getline and excluded done from the list, closes #2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using std::cout, std::cin, std::endl, std::string, std::vector;
+using std::getline;
 
 int main(){
-  string input = "";
+  string input;
   vector<string> list;
 
   do{
@@ -14,13 +16,15 @@ int main(){
     else{
       cout << "What is your next favorite?\n";
     }
-    cin >> input;
+    getline(cin,input);
     list.push_back(input);
   }while( input != "done" );
 
   cout << "Your favorite list:\n";
   for(int i = 0; i < list.size(); i++){
     cout << list.at(i) << endl;
+    if(input != "done")
+    cout << endl;
   }
 
   return 0;
