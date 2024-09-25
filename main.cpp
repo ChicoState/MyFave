75387c599184d27dfcd242cf68194c1f462665ee
cmgomes5 <cmgomes@csuchico.edu>
2024-09-25 11:13:15 -0700

    Stopped done being added to end of favorite list

--- main.cpp
+++ main.cpp
@@ -15,8 +15,9 @@ int main(){
       cout << "What is your next favorite?\n";
     }
     getline(cin, input);
-    // cin >> input;
-    list.push_back(input);
+    
+    if( input != "done")
+      list.push_back(input);
   }while( input != "done" );
 
   cout << "Your favorite list:\n";
