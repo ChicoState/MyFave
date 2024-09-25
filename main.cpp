406443b12f17f61df789a60dfc5c1858cc87aa76
Miguel Ocampo Paniagua <miguelocampo32@gmail.com>
2024-09-25 11:19:49 -0700

    updating the main.cpp file closing #2

--- main.cpp
+++ main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <vector>
 
-using std::cout, std::cin, std::endl, std::string, std::vector;
+using std::cout, std::cin, std::endl, std::string, std::vector, std::getline;
 
 int main(){
   string input = "";
   vector<string> list;
 
   do{
-    if( list.size() == 0 ){
+    if( list.empty() ){
       cout << "What is your favorite?\n";
     }
     else{
       cout << "What is your next favorite?\n";
     }
-    cin >> input;
-    list.push_back(input);
-  }while( input != "done" );
+    //cin >> input;
+    getline(cin,input);
+    if(input != "done" && input != "Done"){
+      list.push_back(input);
+    }
+  }
+  while( input != "done" && input != "Done" );
 
   cout << "Your favorite list:\n";
   for(int i = 0; i < list.size(); i++){
