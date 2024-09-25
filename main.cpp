3cde482692a7b96aa650cf809c222f758e10ac90
chrisheesh <ceverett1@csuchico.edu>
2024-09-25 11:22:08 -0700

    closes #2

--- main.cpp
+++ main.cpp
@@ -14,10 +14,14 @@ int main(){
     else{
       cout << "What is your next favorite?\n";
     }
-    cin >> input;
+    
+    std::getline(std::cin, input);
+    //cin >> input;
     list.push_back(input);
   }while( input != "done" );
 
+  list.pop_back();
+  
   cout << "Your favorite list:\n";
   for(int i = 0; i < list.size(); i++){
     cout << list.at(i) << endl;
