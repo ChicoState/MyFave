3ed65bb4f8dc03d00d9524357da9921bc8dd461a
dac098 <me@dac098.me>
2024-09-25 11:16:40 -0700

    updates to fix #2
    added in the ability to retrieve single line input from the user. also
    checking if the input give is the word done and not adding it to the
    list.

--- main.cpp
+++ main.cpp
@@ -3,25 +3,34 @@
 
 using std::cout, std::cin, std::endl, std::string, std::vector;
 
-int main(){
-  string input = "";
-  vector<string> list;
+int main() {
+    string input = "";
+    vector<string> list;
 
-  do{
-    if( list.size() == 0 ){
-      cout << "What is your favorite?\n";
+    for (;;) {
+        if (list.size() == 0) {
+            cout << "What is your favorite?\n";
+        } else {
+            cout << "What is your next favorite?\n";
+        }
+
+        if (!std::getline(cin, input)) {
+            std::cerr << "error when retrieving input from stdin\n";
+            return 1;
+        }
+
+        if (input == "done") {
+            break;
+        }
+
+        list.push_back(input);
     }
-    else{
-      cout << "What is your next favorite?\n";
+
+    cout << "Your favorite list:\n";
+
+    for (int i = 0; i < list.size(); i++) {
+        cout << list[i] << endl;
     }
-    cin >> input;
-    list.push_back(input);
-  }while( input != "done" );
 
-  cout << "Your favorite list:\n";
-  for(int i = 0; i < list.size(); i++){
-    cout << list.at(i) << endl;
-  }
-
-  return 0;
+    return 0;
 }
