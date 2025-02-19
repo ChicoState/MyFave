302d01a3da65ea5271853026e00804e818546d8a
annasehgal <annasehgal05@gmail.com>
2025-02-19 11:41:15 -0800

    Fixes for DONE

--- main.cpp
+++ main.cpp
@@ -19,13 +19,19 @@ int main(){
     }
     getline(cin,input);
     favorites.push_back(input);
+
+    if(input == "DONE") {
+      favorites.pop_back();
+    }
   }while( input != "DONE" );
 
   cout << "Your favorite list:\n";
-  for(int i = 0; i < favorites.size() -1; i++)
+  // for(int i = 0; i < favorites.size() -1; i++)
+  for(int i = 0; i < favorites.size(); i++)
   {
     cout << favorites.at(i) << endl;
   }
+
 
   return 0;
 }
