7db975acbdb83974ca6d9704679a03cbd0810f3b
Patrick Guevarra <ypvguevarra@csuchico.edu>
2025-02-19 11:30:12 -0800

    Got rid of DONE

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main(){
   }while( input != "DONE" );
 
   cout << "Your favorite list:\n";
-  for(int i = 0; i < favorites.size() -1; i++)
+  for(int i = 0; i < favorites.size() - 1; i++)
   {
     cout << favorites.at(i) << endl;
   }
