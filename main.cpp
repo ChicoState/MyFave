1e566a527a4258abfc360b7dea551ca5108a4324
Jacob Buhler <jacob.buhler@icloud.com>
2025-02-19 11:20:41 -0800

    fixes #69, removed unnecessary stored information

--- main.cpp
+++ main.cpp
@@ -18,6 +18,9 @@ int main(){
       cout << "What is your next favorite?\n";
     }
     getline(cin,input);
+    if(input == "DONE"){
+    	break;
+    }
     favorites.push_back(input);
   }while( input != "DONE" );
 
