3775eec66ff517e7903d7c1bf5d9c437d64817ca
ymadan <ymadan@csuchico.edu>
2025-02-19 11:33:46 -0800

    Fixed issue #69: Prevent storing 'DONE' and added CI configuration

--- main.cpp
+++ main.cpp
@@ -3,29 +3,34 @@
 
 using std::cout, std::cin, std::endl, std::string, std::vector;
 
-int main(){
-  string input = "";
-  vector<string> favorites;
+int main() {
+    string input = "";
+    vector<string> favorites;
 
-  cout << "At any time, type DONE to stop recording favorites.\n";
+    cout << "At any time, type DONE to stop recording favorites.\n";
 
-  do
-  {
-    if( favorites.size() == 0 ){
-      cout << "What is your favorite?\n";
+    do {
+        if (favorites.size() == 0) {
+            cout << "What is your favorite?\n";
+        } else {
+            cout << "What is your next favorite?\n";
+        }
+        
+        getline(cin, input);
+        
+        // Only add to the list if the input is not "DONE"
+        if (input != "DONE") {
+            favorites.push_back(input);
+        }
+        
+    } while (input != "DONE");
+
+    cout << "Your favorite list:\n";
+    
+    // Print the entire list
+    for (int i = 0; i < favorites.size(); i++) {
+        cout << favorites.at(i) << endl;
     }
-    else{
-      cout << "What is your next favorite?\n";
-    }
-    getline(cin,input);
-    favorites.push_back(input);
-  }while( input != "DONE" );
 
-  cout << "Your favorite list:\n";
-  for(int i = 0; i < favorites.size() -1; i++)
-  {
-    cout << favorites.at(i) << endl;
-  }
-
-  return 0;
+    return 0;
 }
