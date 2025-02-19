014cc1cae27cdb1df311d490ae51bcb6000ed313
Prateek Ajith <psajith>
2025-02-19 11:42:25 -0800

    Fixes: 68 - update CI g++ command & 69 - Remove 'DONE' from list

--- main.cpp
+++ main.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
 #include <vector>
 
-using std::cout, std::cin, std::endl, std::string, std::vector;
+using namespace std;
 
 int main()
 {
   string input = "";
-  vector <string> favorites;
-
+  vector<string> favorites;
   cout << "At any time, type DONE to stop recording favorites.\n";
 
-  do
+  while (true)
   {
-    if( favorites.size() == 0 )
-      cout << "What is your favorite?\n";
-
-    else
-      cout << "What is your next favorite?\n";
-  
-    getline(cin,input);
+    if (favorites.empty()) cout << "What is your favorite?\n"; else cout << "What is your next favorite?\n";
+    getline(cin, input);
+    if (input == "DONE") break;
     favorites.push_back(input);
-  }while( input != "DONE" );
+  }
 
   cout << "Your favorite list:\n";
-  for(int i = 0; i < favorites.size() -1; i++)
-    cout << favorites.at(i) << endl;
+  for (int i = 0; i < favorites.size(); i++)
+      cout << favorites.at(i) << endl;
 
   return 0;
 }
