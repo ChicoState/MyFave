06f154cf0e976296f7ffa9963b387dd0ab73b22a
Patrick Guevarra <ypvguevarra@csuchico.edu>
2025-02-19 11:43:12 -0800

    Fixed pull request merge conflict

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main(){
 
   cout << "Your favorite list:\n";
   
-  for(int i = 0; i < favorites.size() - 1; i++)
+  for(int i = 0; i < favorites.size() -1; i++)
   {
     cout << favorites.at(i) << endl;
   }
