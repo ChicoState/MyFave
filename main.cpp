db21b7d6445ba8ffc3a49697d8cecc7b97a4673d
Jack Mumford <jack@cmumford.com>
2025-02-19 11:19:29 -0800

    setup CI and fixed the data management

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 using std::cout, std::cin, std::endl, std::string, std::vector;
 
-int main(){
+int main()
+{
   string input = "";
   vector<string> favorites;
 
@@ -11,18 +12,23 @@ int main(){
 
   do
   {
-    if( favorites.size() == 0 ){
+    if (favorites.size() == 0)
+    {
       cout << "What is your favorite?\n";
     }
-    else{
+    else
+    {
       cout << "What is your next favorite?\n";
     }
-    getline(cin,input);
-    favorites.push_back(input);
-  }while( input != "DONE" );
+    getline(cin, input);
+    if (input != "DONE")
+    {
+      favorites.push_back(input);
+    }
+  } while (input != "DONE");
 
   cout << "Your favorite list:\n";
-  for(int i = 0; i < favorites.size() -1; i++)
+  for (int i = 0; i < favorites.size(); i++)
   {
     cout << favorites.at(i) << endl;
   }
