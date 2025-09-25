f7ecbd3d5ae2968896ab31a32fc73b47a01c7bd0
Vincent Roberson <89275572+bincent0929@users.noreply.github.com>
2025-09-25 20:11:20 +0000

    adds gitignore closes #69 	new file:   .gitignore 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -5,25 +5,31 @@ using std::cout, std::cin, std::endl, std::string, std::vector;
 
 int main()
 {
-  string input = "";
+  string input;
   vector <string> favorites;
 
   cout << "At any time, type DONE to stop recording favorites.\n";
 
-  do
+  bool first_run = true;
+  while (input != "DONE")
   {
+    if (first_run == false) {
+      favorites.push_back(input);
+    }
+    else {
+      first_run = false;
+    }
+    
     if( favorites.size() == 0 )
       cout << "What is your favorite?\n";
-
     else
       cout << "What is your next favorite?\n";
   
     getline(cin,input);
-    favorites.push_back(input);
-  }while( input != "DONE" );
+  }
 
   cout << "Your favorite list:\n";
-  for(int i = 0; i < favorites.size() -1; i++)
+  for(int i = 0; i < favorites.size(); i++) // whole list printed
     cout << favorites.at(i) << endl;
 
   return 0;
